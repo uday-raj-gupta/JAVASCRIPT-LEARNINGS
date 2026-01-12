f06bf40c49b28270b79015484e8c8400bf8701fc
Uday Raj Gupta <udayrajguptabth@gmail.com>
2026-01-12 23:11:07 +0530

    Add files via upload

---
+++ Variables and Data Type/script.js
@@ -0,0 +1,36 @@
+console.log("Hello Its Uday ")
+var a = 15;
+var b = 133.884;
+var c = "Hello";
+var d = 'X';
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+c = a+b;
+console.log("sum of a and b =" ,c);
+const a1 = 18;
+// a1 = 1888;
+// console.log(a)
+//above thing is not allowed
+// let is block code which has its work in a block of code 
+let z=77;
+console.log(z);
+{
+    let z=18373;
+    console.log(z);
+}
+console.log(z);
+// in the above  block only the things changed
+//var changes or updates the value
+
+var y =98;
+console.log(y);
+{
+    var y =888;
+    console.log(y);
+}
+let a="Uday Gupta";
+let b="YP LogiX  Yt Channel";
+let c= "subsrribe";
+console.log(a,b,c);
